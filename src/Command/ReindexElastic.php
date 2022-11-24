f0afdb89b48e22dc03d75ab4ac3aabd7c68decb3
Quentin Barloy <quentin@les-tilleuls.coop>
2022-11-24 23:32:25 +0100

    feat: Add reindex command

--- src/Command/ReindexElastic.php
+++ src/Command/ReindexElastic.php
@@ -17,35 +17,152 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 class ReindexElastic extends Command
 {
     private const MAPPING = [
+        'settings' => [
+            'number_of_shards' => 1,
+            'number_of_replicas' => 0,
+        ],
+        'mappings' => [
+            'dynamic' => 'strict',
+            'properties' => [
+                'author' => [
+                    'type' => 'object',
+                    'properties' => [
+                        'title' => ['type' => 'keyword'],
+                        'fullname' => ['type' => 'text', 'analyzer' => 'french',  'fields' => ['keyword' => ['type' => 'keyword']]],
+                        'email' => ['type' => 'keyword'],
+                    ]
+                ],
+                'edition' => [
+                    'type' => 'object',
+                    'properties' => [
+                        'name' => ['type' => 'text', 'analyzer' => 'french',  'fields' => ['keyword' => ['type' => 'keyword']]],
+                        'location' => ['type' => 'geo_point'],
+                    ]
+                ],
+                'isbn' => ['type' => 'keyword'],
+                'title' => ['type' => 'text', 'analyzer' => 'french',  'fields' => ['keyword' => ['type' => 'keyword']]],
+                'description' => ['type' => 'text', 'analyzer' => 'french'],
+                'category' => ['type' => 'text', 'analyzer' => 'french',  'fields' => ['keyword' => ['type' => 'keyword']]],
+                'subCategory' => ['type' => 'text', 'analyzer' => 'french',  'fields' => ['keyword' => ['type' => 'keyword']]],
+                'releaseDate' => ['type' => 'date'],
+                'price' => ['type' => 'float'],
+                'sales' => ['type' => 'integer'],
+                'ratings' => [
+                    'type' => 'nested',
+                    'properties' => [
+                        'rating' => ['type' => 'integer'],
+                        'username' => ['type' => 'keyword'],
+                    ]
+                ]
+            ]
+        ]
     ];
     private const ALIAS = 'library';
     private const BATCH = 100;
 
+    private Client $elastic;
     private SymfonyStyle $io;
 
-    public function __construct(
-        private Client $elastic,
-        private EntityManagerInterface $entityManager
-    ) {
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
+        $this->elastic = new Client('elasticsearch:9200');
+
         parent::__construct();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $index = $this->elastic->getIndex('library-datedujour');
+        $this->io = new SymfonyStyle($input, $output);
 
-//        Etape 1 création de l'index avec son mapping
-//        $index->create(...);
+        $newIndex = $this->createIndex();
+        $this->indexData($newIndex->getName());
 
-//        Etape 2 ajout d'un alias "library_indexing"
-//        $index->addAlias()
+        $indexNames = $this->elastic->getCluster()->getIndexNames();
 
-//        Etape 3 indexation de tous les livres stockés dans PostgreSQL
+        foreach ($indexNames as $indexName) {
+            if (
+                str_starts_with($indexName, self::ALIAS)
+                && $indexName !== $newIndex->getName()
+            ) {
+                $oldIndex = $this->elastic->getIndex($indexName);
+                $oldIndex->hasAlias(self::ALIAS)
+                    ? $oldIndex->removeAlias(self::ALIAS) && $indexToRemove = $oldIndex
+                    : $oldIndex->delete()
+                ;
+            }
+        }
 
-//        Etape 4 suppression de l'alias "library_indexing"
+        $newIndex->addAlias(self::ALIAS);
+        $newIndex->removeAlias(self::ALIAS.'_indexing');
 
-//        Etape 5 ajout de l'alias "library"
+        if (isset($indexToRemove)) {
+            $this->io->info("DELETING OLD INDEX \"{$indexToRemove->getName()}\"");
+            $indexToRemove->delete();
+        }
 
         return Command::SUCCESS;
     }
+
+    private function createIndex(): Index
+    {
+        $now = new \DateTimeImmutable();
+        $indexName = self::ALIAS.'_'.$now->format('Y-m-d-His');
+        $this->io->info("CREATING INDEX \"$indexName\"");
+
+        $index = $this->elastic->getIndex($indexName);
+        $index->create(self::MAPPING);
+        $index->addAlias(self::ALIAS.'_indexing');
+
+        return $index;
+    }
+
+    private function indexData(string $indexName): void
+    {
+        $this->io->info("INDEXING DATA TO \"$indexName\"");
+        $this->io->progressStart(
+            $this->entityManager->getRepository(Book::class)->count([])
+        );
+
+        $q = $this->entityManager->getRepository(Book::class)
+            ->createQueryBuilder('b')
+            ->getQuery()
+        ;
+
+        $documents = [];
+        /** @var Book $book */
+        foreach ($q->toIterable() as $book) {
+            $documents[] = ['index' => ['_index' => $indexName]];
+            $documents[] = [
+                'author' => $book->getAuthor()->toArray(),
+                'edition' => $book->getEdition()->toArray(),
+                'isbn' => $book->getIsbn(),
+                'title' => $book->getTitle(),
+                'description' => $book->getDescription(),
+                'category' => $book->getSubCategory()->getParent()->getName(),
+                'subCategory' => $book->getSubCategory()->getName(),
+                'releaseDate' => $book->getReleaseDate()->format('Y-m-d'),
+                'price' => $book->getPrice(),
+                'sales' => $book->getSales(),
+                'ratings' => $book->getRatings()->map(
+                    fn(Rating $rating) => $rating->toArray()
+                )->toArray(),
+            ];
+
+            if (0 === (\count($documents) / 2) % self::BATCH) {
+                $this->elastic->bulk($documents);
+                $documents = [];
+            }
+
+            $this->io->progressAdvance();
+            $this->entityManager->detach($book);
+        }
+
+        if ($documents) {
+            $this->elastic->bulk($documents);
+        }
+
+        $this->io->progressFinish();
+    }
+
+
 }
